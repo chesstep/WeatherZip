8d4a7cee095483cb9c74c36657dae1edafbcb7b0
Chesley Stephens <chesley@nibbis.com>
2017-01-30 20:12:50 -0600

    Initial Commit

---
+++ WeatherZip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherZip
+//
+//  Created by Chesley Stephens on 1/30/17.
+//  Copyright © 2017 Nibbis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
